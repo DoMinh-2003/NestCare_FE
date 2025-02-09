55788b45c80e458e87488f1d979f89624ca25a9e
DoMinh-2003 <minhlola28@gmail.com>
2025-02-09 19:10:35 +0700

    Feat/Minh--auth-router

--- src/config/router.tsx
+++ src/config/router.tsx
@@ -1,4 +1,47 @@
-import { createBrowserRouter } from "react-router-dom";
+import { ReactNode } from "react";
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import { useCurrentUser } from "../utils/getcurrentUser";
+import { toast } from "react-toastify";
+import AdminLayout from "../components/layouts/admin-layout";
+import { ADMIN_ROUTES, DOCTOR_ROUTES } from "../constants/routes";
+import ManageUser from "../pages/admin/manage-user";
+import ManageOverview from "../pages/admin/manage-overview";
+
+interface ProtectedRouteByRoleProps {
+  children: ReactNode;
+  allowedRoles: Array<"ADMIN" | "USER">; // Các vai trò cho phép
+}
+
+interface ProtectedRouteAuthProps {
+  children: ReactNode;
+}
+
+const ProtectedRouteAuth: React.FC<ProtectedRouteAuthProps> = ({
+  children,
+}) => {
+  const user = useCurrentUser();
+
+  if (!user) {
+    toast.error("You need to login first!!");
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};
+
+const ProtectedRouteByRole: React.FC<ProtectedRouteByRoleProps> = ({
+  children,
+  allowedRoles,
+}) => {
+  const user = useCurrentUser();
+
+  if (!user || !allowedRoles.includes(user.role)) {
+    toast.error("You do not have permissions to access");
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};
 
 export const router = createBrowserRouter([
   {
@@ -7,6 +50,48 @@ export const router = createBrowserRouter([
   },
   {
     path: "/test",
-    element: <div className="text-3xl font-bold underline ">Hi test</div>,
+    element: (
+      <ProtectedRouteByRole allowedRoles={["ADMIN"]}>
+        <div className="text-3xl font-bold underline ">Hi ADMIN</div>
+      </ProtectedRouteByRole>
+    ),
   },
+  {
+    path: ADMIN_ROUTES.ADMIN,
+    element: (
+      // <ProtectedRouteByRole allowedRoles={["ADMIN"]}>
+        <AdminLayout />
+      // </ProtectedRouteByRole>
+    ),
+    children: [
+      {
+        path: ADMIN_ROUTES.USER,
+        element: <ManageUser />,
+      },
+      {
+        path: ADMIN_ROUTES.OVERVIEW,
+        element: <ManageOverview />,
+      },
+    ],
+  },
+
+  // {
+  //   path: DOCTOR_ROUTES.DOCTOR,
+  //   element: (
+  //     // <ProtectedRouteByRole allowedRoles={["ADMIN"]}>
+  //       <DashboardLayout />
+  //     // </ProtectedRouteByRole>
+  //   ),
+  //   children: [
+  //     {
+  //       path: DOCTOR_ROUTES.USER,
+  //       element: <ManageUser />,
+  //     },
+  //     // {
+  //     //   path: ADMIN_ROUTES.OVERVIEW,
+  //     //   element: <ManageOverview />,
+  //     // },
+  //   ],
+  // },
+
 ]);
