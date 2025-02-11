071ddcab2b3e996b134369d9019662d0d70c4433
TanHuynh68 <tanhpce171112@fpt.edu.vn>
2025-02-11 20:23:47 +0700

    Feat/Tan-contact-page

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,10 +1,14 @@
+import BookingSection from "../../components/organisms/booking-section/BookingSection"
 import ContactWithUs from "../../components/organisms/contact-with-us/ContactWithUs"
+import LearnMoreAboutNestCare from "../../components/organisms/learn-more-about-nest-care/LearnMoreAboutNestCare"
 
 const Contact = () => {
     return (
         <div className="mx-5">
             <div className="container mx-auto">
                 <ContactWithUs />
+                <BookingSection/>
+                <LearnMoreAboutNestCare/>
             </div>
         </div>
     )
