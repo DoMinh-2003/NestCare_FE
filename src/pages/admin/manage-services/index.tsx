69bdb524a7cc7fa58260068609fd28c2db19fc59
TanHuynh68 <tanhpce171112@fpt.edu.vn>
2025-03-10 19:29:59 +0700

    Feat/Tan-Update-and_delete-modal

--- src/pages/admin/manage-services/index.tsx
+++ src/pages/admin/manage-services/index.tsx
@@ -1,120 +1,157 @@
 import { useEffect, useState } from "react";
 import useServiceService, { Service } from "../../../services/useServiceService";
 import { Table, Input, Button, message } from "antd";
-import { SearchOutlined } from '@ant-design/icons';
+import { DeleteOutlined, EditOutlined, SearchOutlined } from '@ant-design/icons';
 import ModalCreateUpdateServices from "../../../components/organisms/modal-create-update-service";
+import ModalDelete from "../../../components/organisms/modal-delete";
 
 const ManagerServices = () => {
-  const [services, setServices] = useState<Service[]>([]);
-  const [searchText, setSearchText] = useState<string>("");
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedService, setSelectedService] = useState(null);
-  const { createServices } = useServiceService()
+    const [services, setServices] = useState<Service[]>([]);
+    const [searchText, setSearchText] = useState<string>("");
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedService, setSelectedService] = useState<Service | null>(null);
+    const { createServices, updateServices, getServices, deleteServices } = useServiceService();
+    const [isModalDeleteOpen, setIsModalDeleteOpen] = useState(false);
 
-  const handleOpenModal = (service = null) => {
-    setSelectedService(service);
-    setIsModalOpen(true);
-  };
+    useEffect(() => {
+        getServicesFromCustomer();
+    }, []);
 
-  const handleSaveService = async (values: any) => {
-    console.log("Dữ liệu gửi lên:", values);
-    // Gửi dữ liệu lên API
-    const response = await createServices(values)
-    if (response && response.data) {
-      setServices(response.data)
-      message.success("Tạo service thành công")
-      getServicesFromCustomer()
-    }
-  };
-  const { getServices } = useServiceService();
+    const handleOpenModal = (service?: Service) => {
+        setSelectedService(service || null);
+        setIsModalOpen(true);
+    };
 
-  useEffect(() => {
-    getServicesFromCustomer();
-  }, []);
+    const handleOpenModalDelete = (service: Service) => {
+        setSelectedService(service);
+        setIsModalDeleteOpen(true);
+    };
 
-  const getServicesFromCustomer = async () => {
-    const response = await getServices();
-    console.log("API Response:", response); // Log toàn bộ response để kiểm tra
+    const handleOkModalDelete = async () => {
+        if (!selectedService) return; // Ensure selectedService is defined
+        const response = await deleteServices(selectedService.id);
+        if (response) {
+            message.success(`Xóa dịch vụ ${selectedService.name} thành công`);
+            setSelectedService(null);
+            setIsModalDeleteOpen(false);
+            getServicesFromCustomer(); // Refresh the service list after deletion
+        } else {
+            message.error(`Xóa dịch vụ ${selectedService.name} thất bại`);
+        }
+    };
 
-    if (response && Array.isArray(response.data)) {
-      setServices(response.data);
-    } else {
-      console.error("Expected an array but got:", response.data);
-      setServices([]); // Đảm bảo luôn truyền một mảng vào Table
-    }
-  };
+    const handleCancelModalDelete = () => {
+        setIsModalDeleteOpen(false);
+    };
 
-  const handleSearch = (value: string) => {
-    setSearchText(value);
-  };
+    const handleSaveService = async (values: Service) => {
+        if (!values.id) {
+            const response = await createServices(values);
+            if (response && response.data) {
+                setServices(response.data);
+                message.success("Tạo service thành công");
+                getServicesFromCustomer();
+            }
+        } else {
+            const response = await updateServices(values);
+            if (response && response.data) {
+                setServices(response.data);
+                message.success("Cập nhật service thành công");
+                getServicesFromCustomer();
+            }
+        }
+    };
 
-  // const filteredServices = Array.isArray(services)
-  //   ? services.filter(service =>
-  //     service.name.toLowerCase().includes(searchText.toLowerCase())
-  //   )
-  //   : [];
+    const getServicesFromCustomer = async () => {
+        const response = await getServices();
+        console.log("res: ", response)
+        if (response && Array.isArray(response.data)) {
+            setServices(response.data.filter((item : Service)=> item.isDeleted === 0));
+        } else {
+            console.error("Expected an array but got:", response.data);
+            setServices([]); // Ensure an array is always passed to Table
+        }
+    };
 
-  const columns = [
-    {
-      title: 'ID',
-      dataIndex: 'id',
-      key: 'id',
-    },
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: 'Price',
-      dataIndex: 'price',
-      key: 'price',
-    },
-    {
-      title: 'Description',
-      dataIndex: 'description',
-      key: 'description',
-    },
-    {
-      title: 'Created At',
-      dataIndex: 'createdAt',
-      key: 'createdAt',
-    },
-    {
-      title: 'Updated At',
-      dataIndex: 'updatedAt',
-      key: 'updatedAt',
-    },
-  ];
+    const handleSearch = (value: string) => {
+        setSearchText(value);
+    };
 
-  return (
-    <div>
-      <ModalCreateUpdateServices
-        visible={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        onSubmit={handleSaveService}
-        initialValues={selectedService}
-      />
-      <div style={{ marginBottom: 16 }}>
-        <Input
-          placeholder="Search by name"
-          value={searchText}
-          onChange={(e) => handleSearch(e.target.value)}
-          style={{ width: 200, marginRight: 8 }}
-          suffix={<SearchOutlined />}
-        />
-        <Button type="primary" onClick={() => handleOpenModal()}>
-          Thêm dịch vụ
-        </Button>
-      </div>
-      <Table
-        dataSource={Array.isArray(services) ? services : []}
-        columns={columns}
-        rowKey="id"
-      />
-    </div>
-  );
+    const columns = [
+        {
+            title: 'ID',
+            dataIndex: 'id',
+            key: 'id',
+        },
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Price',
+            dataIndex: 'price',
+            key: 'price',
+        },
+        {
+            title: 'Description',
+            dataIndex: 'description',
+            key: 'description',
+        },
+        {
+            title: 'Created At',
+            dataIndex: 'createdAt',
+            key: 'createdAt',
+        },
+        {
+            title: 'Updated At',
+            dataIndex: 'updatedAt',
+            key: 'updatedAt',
+        },
+        {
+            title: 'Action',
+            render: (record: Service) => (
+                <div className="flex gap-2">
+                    <EditOutlined onClick={() => handleOpenModal(record)} className="text-blue" />
+                    <DeleteOutlined onClick={() => handleOpenModalDelete(record)} className="text-red-500" />
+                </div>
+            )
+        },
+    ];
+
+    return (
+        <div>
+            <ModalDelete
+                handleCancelModalDelete={handleCancelModalDelete}
+                handleOkModalDelete={handleOkModalDelete}
+                name={selectedService?.name || ""}
+                isModalOpenDelete={isModalDeleteOpen}
+            />
+            <ModalCreateUpdateServices
+                visible={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+                onSubmit={()=>handleSaveService} // Pass the function directly
+                initialValues={selectedService}
+            />
+            <div style={{ marginBottom: 16 }}>
+                <Input
+                    placeholder="Search by name"
+                    value={searchText}
+                    onChange={(e) => handleSearch(e.target.value)}
+                    style={{ width: 200, marginRight: 8 }}
+                    suffix={<SearchOutlined />}
+                />
+                <Button type="primary" onClick={() => handleOpenModal()}>
+                    Thêm dịch vụ
+                </Button>
+            </div>
+            <Table
+                dataSource={Array.isArray(services) ? services : []}
+                columns={columns}
+                rowKey="id"
+            />
+        </div>
+    );
 };
 
 export default ManagerServices;
-
