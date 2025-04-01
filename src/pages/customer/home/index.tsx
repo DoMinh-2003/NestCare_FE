8e8e27b880d20fe2ce4f5ae1025faf7655ee18da
DoMinh-2003 <minhlola28@gmail.com>
2025-04-02 02:48:01 +0700

    Feat/Minh chatbot

--- src/pages/customer/home/index.tsx
+++ src/pages/customer/home/index.tsx
@@ -1,6 +1,7 @@
 import About from "../../../components/molecules/about/About";
 import HomeCarousel from "../../../components/molecules/home-carousel";
 import BookingSection from "../../../components/organisms/booking-section/BookingSection";
+import ChatbotWidget from "../../../components/organisms/chatbot";
 import CustomerFeedback from "../../../components/organisms/customer-feedback/CustomerFeedback";
 import DoctorList from "../../../components/organisms/doctor-list/DoctorList";
 import HealthKnowledge from "../../../components/organisms/health-knowledge/HealthKnowledge";
@@ -24,6 +25,7 @@ const HomePage = () => {
         <HealthKnowledge />
         <InformationForCustomer />
         <LearnMoreAboutNestCare />
+        <ChatbotWidget/>
       </div>
     </div>
   );
